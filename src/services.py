69dc2aff4f61b82b9e4a6a0ff9afe846a2e04969
Darya Balba <dpbalba@edu.hse.ru>
2023-12-09 14:22:07 +0300

    upd tests

--- src/services.py
+++ src/services.py
@@ -1,6 +1,6 @@
 from enum import Enum, auto
 from collections import namedtuple
-from benchmarl import debug_decorator
+from src.benchmarl import debug_decorator
 
 
 Salary = namedtuple(typename='Salary',
